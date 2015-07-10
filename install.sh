bf443fb08c95f305294968ccf123b4f5a5365d72
Ali Craigmile <ali@craigmile.com>
2015-07-10 18:21:59 +0100

    don't forget to install blink

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ install -m 0755 sbin/pushthebuttond /usr/local/sbin/
 install -m 0755 bin/loop-dashboards /usr/local/bin/
 install -m 0755 bin/speak-the-time /usr/local/bin/
 install -m 0755 bin/launch-xeyes /usr/local/bin/
+install -m 0755 bin/blink /usr/local/bin/
 install -m 0644 html/index.html /var/www/
 
