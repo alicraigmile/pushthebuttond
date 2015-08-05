b5abb703e0ca1064dfb56b84fa189017db15fa30
Ali Craigmile <ali@craigmile.com>
2015-08-05 23:37:41 +0100

    config now loaded from sbin/pushthebuttond.cfg

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -x
 
 install -m 0755 init.d/pushthebuttond /etc/init.d/
 install -m 0755 sbin/pushthebuttond /usr/local/sbin/
+install -m 0644 sbin/pushthebuttond.cfg /usr/local/sbin/
 install -m 0755 bin/loop-dashboards /usr/local/bin/
 install -m 0755 bin/speak-the-time /usr/local/bin/
 install -m 0755 bin/launch-xeyes /usr/local/bin/
